1e58afd7d2561f3ea4cba537d028102579fa0382
Gustavo <GustavoSanMartin99@gmail.com>
2018-05-16 20:23:51 -0400

    Beautified Code

--- Main.js
+++ Main.js
@@ -1,28 +1,30 @@
 var map;
 var marker = false; //is there an active marker on the screen?
-var origin = null;
 var directionsService = null;
 var dest = null;
 var commuteTime = "0";
 var currentOriginLocation = null;
 
+/**
+ * Called from html file
+ * Creates a new instance for the map API and directions API.
+ */
 function initMap() {
-
-
+    //create an instance of the map
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 45.4085, lng: -75.700},
         zoom: 13
     });
 
-    origin = new google.maps.LatLng(45.4085, -75.700);
+    //create an instance of the directions API
     directionsService = new google.maps.DirectionsService();
 
     //Listen for any clicks on the map.
     google.maps.event.addListener(map, 'click', function(event) {
-        //Get the location that the user clicked.
+        //Get the location of where the user clicked
         var destLocation = event.latLng;
 
-        //If the marker hasn't been added.
+        //If there is no marker on the screen already
         if(marker === false){
             //Create the marker.
             marker = new google.maps.Marker({
@@ -39,25 +41,21 @@ function initMap() {
             //Marker has already been added, so just change its location.
             marker.setPosition(destLocation);
         }
-        //Get the marker's location.
+        //display and save the marker's coordinates
         markerLocation();
     });
-
-
 }
 
 /**
- * every time the marker's position changes
+ * Called every time the marker's position changes. Display coordinates on the screen.
  */
 function markerLocation(){
     //Get location.
-    var currentLocation = marker.getPosition();
-    dest = currentLocation;
-    //console.log(dest);
-    //Add lat and lng values to a field that we can save.
+    dest = marker.getPosition();
 
-    document.getElementById('lat').value = currentLocation.lat(); //latitude
-    document.getElementById('lng').value = currentLocation.lng(); //longitude
+    //display the marker's coordinates on the screen
+    document.getElementById('lat').value = dest.lat(); //latitude
+    document.getElementById('lng').value = dest.lng(); //longitude
 }
 
 /**
@@ -65,51 +63,56 @@ function markerLocation(){
  * Calculates time to destination
  */
 function calcRoute() {
-
     var travelMode = document.getElementById("travelMode").value;
-    //console.log(travelMode);
-    //console.log(dest, "yo");
+
     if (!dest){
         return;
     }
-    var count = 0;
+    //loop through the map coordinates in the given area
     for (var lat = 45.41; lat < 45.43; lat+=0.005){
         for (var long = -75.7; long < -75.65; long+=0.005) {
-            count++;
-            console.log(count);
             currentOriginLocation = new google.maps.LatLng(lat, long);
 
+            //directions request
             var request = {
                 origin: currentOriginLocation,
                 destination: dest,
                 travelMode: travelMode
             };
             directionsService.route(request, function (result, status) {
+                console.log(status);
+                //direction results returned successfully
                 if (status === 'OK') {
                     commuteTime = result.routes[0].legs[0].duration.text;
-                    console.log(commuteTime);
                     addMarker(result.request.origin.location, commuteTime);
                 }
+                //API was called too many times in 1 second
                 else if (status==='OVER_QUERY_LIMIT'){
-                    console.log("before");
-                    sleep(2000);
-                    console.log("after");
+                    sleep(2000); //wait until allowed to make more API calls
                 }
             });
 
         }
     }
-
-
 }
-function addMarker(location, time) {
-    console.log(time);
+
+/**
+ * Adds a marker to the map
+ * @param location Google LatLng location object
+ * @param label Text to be displayed next to marker
+ */
+function addMarker(location, label) {
     new google.maps.Marker({
         position: location,
-        label: time,
+        label: label,
         map: map
     });
 }
+
+/**
+ * Freezes all processes
+ * @param milliseconds How long to freeze for
+ */
 function sleep(milliseconds) {
     var start = new Date().getTime();
     for (var i = 0; i < 1e7; i++) {
@@ -118,6 +121,3 @@ function sleep(milliseconds) {
         }
     }
 }
-
-//Load the map when the page has finished loading.
-google.maps.event.addDomListener(window, 'load', initMap);
